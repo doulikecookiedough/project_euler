5d412f742ceee5070a5fc2c84479303b3111e374
Dou Mok <doumok@Dous-MacBook-Pro.local>
2022-10-11 09:58:10 -0700

    Cleaned up code

--- problem1/pjeu_p1.py
+++ problem1/pjeu_p1.py
@@ -1,11 +1,11 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def get_multiple(val1, val2):
+def get_multiple(val1, val2, limit):
     counter = 1
     sum = 0
 
-    while counter < 1000:
+    while counter < limit:
         quotient_one = counter % val1
         quotient_two = counter % val2
         if quotient_one == 0 or quotient_two == 0:
@@ -16,5 +16,9 @@ def get_multiple(val1, val2):
 
 
 if __name__ == '__main__':
-    result = get_multiple(3, 5)
+    first_value = 3
+    second_value = 5
+    number_limit = 1000
+
+    result = get_multiple(first_value, second_value, number_limit)
     print(result)
