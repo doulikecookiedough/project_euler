d544f0f520f86a14825072088bd09ff46267ef59
Dou Mok <doumok@Dous-MacBook-Pro.local>
2022-10-12 10:50:38 -0700

    Added pseudo code

---
+++ problem3/pjeu_p3.py
@@ -0,0 +1,14 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143?
+def get_largest_prime_factor(num):
+    # Prime numbers can be only divisible by itself and 1
+    # Generate a list of prime numbers
+    # Check to see if provided number can be divided with no remainder
+    # If yes, add it to an approved prime number list
+    # Select the last item in the list (which will be the largest number)
+    print(num)
+
+
+if __name__ == '__main__':
+    get_largest_prime_factor(13195)
+    # get_largest_prime_factor(600851475143)
