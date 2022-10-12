3ea852f7349a95fc975f8f44155a13654e057b1e
Dou Mok <doumok@Dous-MacBook-Pro.local>
2022-10-12 16:48:49 -0700

    Working solution for smaller values, but does not work well with large numbers

--- problem3/pjeu_p3.py
+++ problem3/pjeu_p3.py
@@ -9,19 +9,35 @@ def get_largest_prime_factor(num):
     # Create a new list
     # Select the last item in the list (largest prime factor)
 
-    num_factor_list = []
-    for i in range(1, num + 1):
-        # i is divisibe if there is no remainder
-        if num % i == 0:
-            # print(f"divisible: {i}")
-            num_factor_list.append(i)
+    # num_factor_list = []
+    # for i in range(1, num + 1):
+    #     # i is divisibe if there is no remainder
+    #     if num % i == 0:
+    #         # print(f"divisible: {i}")
+    #         num_factor_list.append(i)
 
     prime_factor_list = []
-    for n in num_factor_list:
-        print(n)
+    for i in range(1, num + 1):
+        if i != 1 and num % i == 0:
+            prime_number_flag = 0
+            for j in range(1, i):
+                if i % j == 0:
+                    if prime_number_flag >= 2:
+                        pass
+                    else:
+                        prime_number_flag += 1
+
+            if prime_number_flag >= 2:
+                prime_number_flag = 0
+                pass
+            else:
+                prime_factor_list.append(i)
+                prime_number_flag = 0
+
+    print(prime_factor_list)
 
 
 if __name__ == '__main__':
-    get_largest_prime_factor(60)
-    # get_largest_prime_factor(13195)
+    # get_largest_prime_factor(60)
+    get_largest_prime_factor(13195)
     # get_largest_prime_factor(600851475143)
