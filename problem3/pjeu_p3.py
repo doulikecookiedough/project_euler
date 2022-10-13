451588bf6cf624080b3538f2c0b0522d32dca50b
Dou Mok <doumok@Dous-MacBook-Pro.local>
2022-10-13 10:33:48 -0700

    Second solution works, but still does not scale well.

--- problem3/pjeu_p3.py
+++ problem3/pjeu_p3.py
@@ -26,9 +26,9 @@ def get_largest_prime_factor_sol_1(num):
     return largest_prime_factor
 
 
+# Still loses effectiveness as N grows larger
+# Try again...
 def get_largest_prime_factor_sol_2(num):
-    # Determine new algorithm to quickly generate a list of prime numbers up to provided number
-
     def check_for_remainder(num, val):
         modulo_check = num % val
         if modulo_check == 0:
@@ -36,25 +36,47 @@ def get_largest_prime_factor_sol_2(num):
         else:
             return False
 
+    def is_prime_number(num):
+        prime_number_flag = 0
+        for i in range(1, num):
+            if num != 1 and num % i == 0:
+                prime_number_flag += 1
+            if prime_number_flag >= 2:
+                return False
+        return True
+
     # Get first divisible factor
     start_value = 2
-    first_divisible_factor = 0
+    largest_divisible_factor = 0
+    largest_prime_factor = 0
 
-    while first_divisible_factor == 0:
+    while largest_divisible_factor == 0:
         status = check_for_remainder(num, start_value)
         if status == True:
-            first_divisible_factor = int(num / start_value)
-            break
+            largest_divisible_factor = int(num / start_value)
+            first_divisible_factor = start_value
         else:
             start_value += 1
             check_for_remainder(num, start_value)
 
-    return first_divisible_factor
+    prime_factor_list = []
+    for p in range(start_value, largest_divisible_factor):
+        if num % p == 0:
+            prime_status = is_prime_number(p)
+            if prime_status == True:
+                prime_factor_list.append(p)
+
+    return prime_factor_list[-1]
+
+
+def get_largest_prime_factor_sol_3(num):
+    # Review prime factor tree
+    print(num)
 
 
 if __name__ == '__main__':
     # result = get_largest_prime_factor(60)
     # result = get_largest_prime_factor_sol_1(13195)
-    result = get_largest_prime_factor_sol_2(600851475143)
+    result = get_largest_prime_factor_sol_2(13195)
     # result = get_largest_prime_factor(600851475143)
     print(result)
