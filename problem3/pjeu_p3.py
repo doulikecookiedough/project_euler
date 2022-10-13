254578ffd498c538fc614b6cd63eb6c607882af2
Dou Mok <doumok@Dous-MacBook-Pro.local>
2022-10-13 13:25:19 -0700

    Efficiently retrieved the largest prime factor given any number

--- problem3/pjeu_p3.py
+++ problem3/pjeu_p3.py
@@ -2,11 +2,9 @@
 # What is the largest prime factor of the number 600851475143?
 import math
 
-# Loses effectiveness as N grows larger
-# Not a good solution
-
 
 def get_largest_prime_factor_sol_1(num):
+    """Loses effectiveness as N grows larger, not an efficient solution"""
     largest_prime_factor = 1
     for i in range(1, num + 1):
         if i != 1 and num % i == 0:
@@ -20,7 +18,6 @@ def get_largest_prime_factor_sol_1(num):
 
             if prime_number_flag >= 2:
                 prime_number_flag = 0
-                pass
             else:
                 if i > largest_prime_factor:
                     largest_prime_factor = i
@@ -29,9 +26,8 @@ def get_largest_prime_factor_sol_1(num):
     return largest_prime_factor
 
 
-# Still loses effectiveness as N grows larger
-# Try again...
 def get_largest_prime_factor_sol_2(num):
+    """Still loses effectiveness as N grows larger, try again..."""
     def check_for_remainder(num, val):
         modulo_check = num % val
         if modulo_check == 0:
@@ -54,23 +50,24 @@ def get_largest_prime_factor_sol_2(num):
 
     while largest_divisible_factor == 0:
         status = check_for_remainder(num, start_value)
-        if status == True:
+        if status is True:
             largest_divisible_factor = int(num / start_value)
         else:
             start_value += 1
             check_for_remainder(num, start_value)
 
     prime_factor_list = []
-    for p in range(start_value, largest_divisible_factor):
-        if num % p == 0:
-            prime_status = is_prime_number(p)
-            if prime_status == True:
-                prime_factor_list.append(p)
+    for dnum in range(start_value, largest_divisible_factor):
+        if num % dnum == 0:
+            prime_status = is_prime_number(dnum)
+            if prime_status is True:
+                prime_factor_list.append(dnum)
 
     return prime_factor_list[-1]
 
 
 def get_largest_prime_factor_sol_3(num):
+    """Use a factor tree to improve performance"""
     # Create a prime factor tree
     # Get the first two factors, then break it down from there
     # Add all the numbers to a list, sort the list
@@ -92,24 +89,37 @@ def get_largest_prime_factor_sol_3(num):
 
     def get_factors(num):
         first_factor = 2
-        second_factor = 0
+        second_factor = num
 
         factor_list = []
+        # Start the process, determine if the provided number is a prime number
+        factor_flag = is_prime(second_factor)
 
-        # Begin the process by getting first two factors
-        while second_factor == 0:
-            status = check_for_remainder(num, first_factor)
-            if status == True:
-                second_factor = int(num / first_factor)
+        # While the number to check is not a prime number, continue to retrieve factors
+        while factor_flag is False:
+            modulo_status = check_for_remainder(second_factor, first_factor)
+            if modulo_status is True:
+                # If the second factor is divisible by the first, add the two factors to the array
+                second_factor = int(second_factor / first_factor)
                 factor_list.append(first_factor)
                 factor_list.append(second_factor)
+                # Then reset the process
+                factor_flag = is_prime(second_factor)
+                first_factor = 2
             else:
+                # If the second factor is not divisible by the first factor, increment the first factor until it is
                 first_factor += 1
                 check_for_remainder(num, first_factor)
 
-        # Now get the rest...
+        # Once we have the factor list, iterate over it and determine the largest prime factor
+        largest_prime_factor = 0
+        for factor in factor_list:
+            prime_status = is_prime(factor)
+            if prime_status:
+                if factor > largest_prime_factor:
+                    largest_prime_factor = factor
 
-        return factor_list
+        return largest_prime_factor
 
     factor_list = get_factors(num)
     return factor_list
@@ -119,5 +129,4 @@ if __name__ == '__main__':
     # result = get_largest_prime_factor(60)
     # result = get_largest_prime_factor_sol_1(13195)
     result = get_largest_prime_factor_sol_3(600851475143)
-    # result = get_largest_prime_factor(600851475143)
     print(result)
